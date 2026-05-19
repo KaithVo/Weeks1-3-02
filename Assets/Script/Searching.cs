d6db770ed58b417d300c8fd6c41f8c70fe76ba86
vokar <vokar@sheridancollege.ca>
2026-05-19 13:11:46 -0400

    Object update the postion when they move  and continue to move from the new position to the next one

--- Assets/Script/Searching.cs
+++ Assets/Script/Searching.cs
@@ -22,5 +22,14 @@ public class Searching : MonoBehaviour
         t += Time.deltaTime * speed; //increasing t over time
 
         transform.position = Vector2.Lerp(starting, ending,t);
+
+        if (t>= 1f)
+        {
+            //Reset t to 0 and set the starting position to the current position, and generate a new random ending position
+            //update start in update because we want to start from the current position, not the original position
+            t = 0f;
+            starting = transform.position;
+            ending = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+        }   
     }
 }
