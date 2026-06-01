a54ba762e2a9cec1ec3c78f59f9f9f5cc851b174
mymacbook <vokar@sheridancollege.ca>
2026-05-31 21:20:34 -0400

    Bug movement in

--- Assets/Script/Assignment1/LetterAniamtion.cs
+++ Assets/Script/Assignment1/LetterAniamtion.cs
@@ -6,7 +6,7 @@ public class LetterAniamtion : MonoBehaviour
     public AnimationCurve animationCurve;
     public float startSize;
     public float endSize;
-
+    public float t; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
