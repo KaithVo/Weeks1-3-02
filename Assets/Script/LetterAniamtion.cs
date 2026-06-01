019e569d935e077b409876dc777e3b92a69592a8
mymacbook <vokar@sheridancollege.ca>
2026-05-31 22:02:13 -0400

    Color working wrongly, size expansion isnt working
    Random color is working but keeps changing color. Expansion isnt working

--- Assets/Script/LetterAniamtion.cs
+++ Assets/Script/LetterAniamtion.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LetterAniamtion : MonoBehaviour
 {
+    private bool mouseIsOver = false;
+    public Color col;
+    public SpriteRenderer spriteRenderer; //for sprite change
 
+<<<<<<< Updated upstream:Assets/Script/LetterAniamtion.cs
     public AnimationCurve animationCurve;
     public float startSize;
     public float endSize;
 
+=======
+    //scales value
+    public Vector3 originalScale;
+    public Vector3 IsExpanding = new Vector3(1.5f, 1.5f, 1.5f);
+>>>>>>> Stashed changes:Assets/Script/Assignment1/LetterAniamtion.cs
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,6 +27,25 @@ public class LetterAniamtion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        //Detect mouse is over the object
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        //if mouse is in the bounds area, change color
+        if (spriteRenderer.bounds.Contains(mousePos) == true)
+        {
+            //use the color variable
+            spriteRenderer.color = Random.ColorHSV();
+            mouseIsOver = true;
+
+
+        }
+        else
+        {
+            //set the color to white
+            spriteRenderer.color = Color.white;
+            mouseIsOver = false;
+        }
+
     }
 }
