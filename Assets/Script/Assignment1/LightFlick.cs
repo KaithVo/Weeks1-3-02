f20460c2b6c21c85354a839e145273dd6aec0527
mymacbook <vokar@sheridancollege.ca>
2026-06-01 18:25:14 -0400

    input light flickering sprite and animation curve, but alpha color isnt working

--- Assets/Script/Assignment1/LightFlick.cs
+++ Assets/Script/Assignment1/LightFlick.cs
@@ -1,16 +1,37 @@
+using System;
 using UnityEngine;
 
 public class LightFlick : MonoBehaviour
 {
+    //use the same method in the special letter but in color
+    //spriteRender but for Alpha this time
+    public SpriteRenderer SpriteRenderer;
+
+    //range for min and max for Alpha
+    public float minAlpha = 0.2f;
+    public float maxAlpha = 0.7f;
+
+    //animationCurve ofcours
+    private float speed = 0.5f;
+    public AnimationCurve flickeringCurve;
+    private float flickerTime;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        flickerTime += Time.deltaTime * speed;
+
+        //aplly alpha witout changing colpr
         
+        currentColor.a = Mathf.Lerp(minAlpha, maxAlpha, flickeringCurve.Evaluate(flickerTime));
+        Color currentColor = currentColor;
+
     }
 }
