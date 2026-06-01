f2d77789c465b076f8185de2c79e30f8279c9f64
mymacbook <vokar@sheridancollege.ca>
2026-06-01 01:21:03 -0400

    Update LetterAniamtion.cs
    Animating curve but not working

--- Assets/Script/Assignment1/LetterAniamtion.cs
+++ Assets/Script/Assignment1/LetterAniamtion.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class LetterAniamtion : MonoBehaviour
 {
+    private bool mouseIsOverMe = false;
 
-    public AnimationCurve animationCurve;
-    public float startSize;
-    public float endSize;
-    public float t; 
+    public Color col;
+    public SpriteRenderer spriteRenderer; //for sprite chnge
+
+    //scales value
+    public Vector3 originalScale;
+    public Vector3 IsExpanding = new Vector3(1.5f, 1.5f, 1.5f);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,6 +22,49 @@ public class LetterAniamtion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Animating();
+        //change color
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        if (spriteRenderer.bounds.Contains(mousePos) == true)
+        {
+            //use the color variable
+            spriteRenderer.color = Random.ColorHSV();
+            mouseIsOverMe = true;
+
+        }
+        else
+        {
+            //set the color to white
+            spriteRenderer.color = Color.white;
+            mouseIsOverMe = false;
+        }
+
+    }
+
+    private float speed = 2f;
+    //time
+    private float t = 0;
+    public AnimationCurve curve;
+
+    //animation curve for size
+    void Animating() //https://zh.esotericsoftware.com/forum/d/3825-unity-random-bone-explosion/2
+    {
+        //if mouse is over than the animation happens
+        if (mouseIsOverMe)
+        {
+            //increase t 
+            t += Time.deltaTime * speed;
+        }
+        else
+        {
+            //decrease t
+            t -= Time.deltaTime * speed;
+
+        }
+
+        //clamp instead of resetting 
+        t = Mathf.Clamp01(t);
+
+        transform.localScale = Vector3.Lerp(originalScale, IsExpanding, curve.Evaluate(t));
     }
 }
